86a5021e5e16a722a63ba3000d5bebc7308c4d58
EmanSza <eszabadka@hotmail.ca>
2023-11-26 13:16:30 -0500

    Minor Fix I missed

--- server/src/Routes/Authentication.js
+++ server/src/Routes/Authentication.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const AuthMiddle = require('../Middleware/AuthMiddleware');
 const authController = require('../Controllers/authController');
 
-router.post('/signin', authController.signin(req, res));
+router.post('/signin', authController.login);
 
 router.get('/signout', AuthMiddle, function(req, res) {
     res.send('Sign out the User');
