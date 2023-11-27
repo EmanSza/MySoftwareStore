512698e0bf30cec08d0bf7a37429a615b3175bf5
EmanSza <eszabadka@hotmail.ca>
2023-11-26 23:02:22 -0500

    Fixed Auth route

--- server/src/Routes/Authentication.js
+++ server/src/Routes/Authentication.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const AuthMiddle = require('../Middleware/AuthMiddleware');
+const AuthMiddle = require('../Middleware/AuthMiddle');
 const authController = require('../Controllers/authController');
 
 router.post('/signin', authController.login);
